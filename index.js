2647eeec847ae6b095dca905b02d9aef8b667d29
rubanero14 <85555894+rubanero14@users.noreply.github.com>
2021-09-08 22:25:12 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-// Import express...
 var express = require('express');
-// I create app server...
 var app = express();
 var port = process.env.PORT || 3000;
-// When user call / api from the browser, I will return Hello world text
 app.get('/', function(req, res){
- res.send("How to create a server using Express.js, view file index.js!");
+ res.send("Hello world!");
 });
-// When user call / api from the browser, I will return Hello everyone text
-app.get('/hello', function (req,res){
-    res.send("Hello Everyone!");
-});
-// When user call /goodbye from the browser, and passes the name as parameter, i will return {{name}}
-app.get('/goodbye/:name', function (req,res){
-    //from the request, get the parameter labeled with name
-    res.send('Goodbye '+req.params.name);
-});
-// When user call /bonjour from an app as a POST
-// I will show bonjour
-app.post('/bonjour', function(req,res){
-    res.send('Bonjour');
-});
-// I will listen to port 3000
+app.get('/:timestamp', function(req,res){
+    res.json({"unix":"to do tomorrow","natural":"to do tomorrow"})
+})
 app.listen(port);
